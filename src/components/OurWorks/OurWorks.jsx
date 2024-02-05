536f8e53203777b58c77fbb90779da646a93220a
Dzmitry <kopyldmitriy@gmail.com>
2024-02-05 23:06:06 +0300

    fix: some bug

--- src/components/OurWorks/OurWorks.jsx
+++ src/components/OurWorks/OurWorks.jsx
@@ -4,19 +4,19 @@ const OurWorks = () => {
 
     const imagesList = [
         {
-            img: './img/3_4.png',
+            img: '../img/3_4.png',
             name: 'Incasol',
             color: 'images_incasol',
             link: 'https://www.amazon.com.mx/Incasol-Vitality-Suplemento-Presi%C3%B3n-Arterial/dp/B0C5N6BXX8/ref=sr_1_1?keywords=incasol&qid=1707055478&sr=8-1&ufe=app_do%3Aamzn1.fos.4e545b5e-1d45-498b-8193-a253464ffa47'
         },
         {
-            img: './img/3_4.png',
+            img: '../img/3_4.png',
             name: 'Incasol',
             color: 'images_lutevid',
             link: 'https://www.amazon.com.mx/Incasol-Vitality-Suplemento-Presi%C3%B3n-Arterial/dp/B0C5N6BXX8/ref=sr_1_1?keywords=incasol&qid=1707055478&sr=8-1&ufe=app_do%3Aamzn1.fos.4e545b5e-1d45-498b-8193-a253464ffa47'
         },
         {
-            img: './img/3_4.png',
+            img: '../img/3_4.png',
             name: 'Incasol',
             color: 'images_incasol',
             link: 'https://www.amazon.com.mx/Incasol-Vitality-Suplemento-Presi%C3%B3n-Arterial/dp/B0C5N6BXX8/ref=sr_1_1?keywords=incasol&qid=1707055478&sr=8-1&ufe=app_do%3Aamzn1.fos.4e545b5e-1d45-498b-8193-a253464ffa47'
