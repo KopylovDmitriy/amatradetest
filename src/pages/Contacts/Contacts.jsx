a0f4f76ac2aedefae9ea90f1be0bdac5fe3a9eed
KopylovDmitriy <kopyldmitriy@gmail.com>
2024-02-07 16:35:40 +0300

    adaptive form

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,6 +1,17 @@
+import { useState } from 'react';
+
 import './Contacts.css';
 
 const Contacts = () => {
+
+    const [name, setName] = useState('');
+
+    const onSubmitName = (e) => {
+        e.preventDefault()
+        setName(e.target.value)
+        console.log(name)
+    }
+
 
     return(
         <div className="page page__contacts ">
@@ -10,11 +21,12 @@ const Contacts = () => {
                     <div className="page__application">
                         <h1 className="page__title">Contact us</h1>
                         <p className="page__subtitle">Our team is on hand to answer your questions within 24 hours.</p>
-                        <form action="post">
+                        <form action="post" onChange={onSubmitName}>
                             <input type="text" />
                             <input type="number" />
                             <input type="text" />
-                            <button className="button button__input">Send message</button>
+                            <button className="button button__input" >Send message</button>
+                            <h2>{name}</h2>
                         </form>
                     </div>
                     <div className="page__contacts">
