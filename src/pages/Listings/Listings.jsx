deccb250dfd8d296790eeae88fa2a09a874f1726
KopylovDmitriy <kopyldmitriy@gmail.com>
2024-02-09 14:23:43 +0300

    adaptive 09/02

--- src/pages/Listings/Listings.jsx
+++ src/pages/Listings/Listings.jsx
@@ -3,7 +3,7 @@ const Listings = () => {
         <div className="page page__contacts ">
             <div className="page__photo"></div>
                 <div className="container">
-                <h1 style={{"padding": "40px 0", "textAlign": "center"}}>Listings</h1>
+                <h1 style={{"padding": "40px 0", "textAlign": "center"}}>Listings 09.02</h1>
             </div>  
         </div>
     )
