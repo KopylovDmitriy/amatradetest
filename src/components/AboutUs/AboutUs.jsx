b97b8ab4c7dea2a471c37ab800da528467561437
Adrian <dmitrukadrian@gmail.com>
2024-05-07 03:21:55 +0300

    feat: teast fonts

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,58 +1,52 @@
-import { useEffect } from 'react';
-import { useState } from 'react';
+import { useEffect } from "react";
+import { useState } from "react";
 
-import { motion } from 'framer-motion';
-import './AboutUs.css'
-
+import { motion } from "framer-motion";
+import "./AboutUs.css";
 
 const AboutUs = () => {
-    // const [num, setNum] = useState(3);
+  // const [num, setNum] = useState(3);
 
-    // useEffect(() => {
-        
-    //     let interval = setInterval(() => {
-    //         setNum(getRandomInt(1, 9));
-    //     }, 50)
+  // useEffect(() => {
 
-    //     setTimeout(() => {
+  //     let interval = setInterval(() => {
+  //         setNum(getRandomInt(1, 9));
+  //     }, 50)
 
-    //     clearInterval(interval)
-    //     setNum(3)
+  //     setTimeout(() => {
 
-    //     }, 2000);
-    // }, [])
+  //     clearInterval(interval)
+  //     setNum(3)
 
-    // function getRandomInt(min, max) {
-    //     min = Math.ceil(min);
-    //     max = Math.floor(max);
-    //     return Math.floor(Math.random() * (max - min + 1)) + min;
-    // }
+  //     }, 2000);
+  // }, [])
 
-    return(
-       
-                <div className="statistic__box">
-                    <ul className="statistic__list">
+  // function getRandomInt(min, max) {
+  //     min = Math.ceil(min);
+  //     max = Math.floor(max);
+  //     return Math.floor(Math.random() * (max - min + 1)) + min;
+  // }
 
-                        <li className="statistic__item">
-                            <h3 className="statistic__title">3+</h3>
-                            <p className="statistic__desc">years of expertise in E-commerce</p>
-                        </li>
+  return (
+    <div className="statistic__box">
+      <ul className="statistic__list">
+        <li className="statistic__item">
+          <h3 className="statistic__title">3+</h3>
+          <p className="statistic__desc">years of expertise in E-commerce</p>
+        </li>
 
-                        <li className="statistic__item">
-                            <h3 className="statistic__title">15+</h3>
-                            <p className="statistic__desc">people in the team</p>
-                        </li>
+        <li className="statistic__item">
+          <h3 className="statistic__title">15+</h3>
+          <p className="statistic__desc">people in the team</p>
+        </li>
 
-                        <li className="statistic__item">
-                            <h3 className="statistic__title">70+</h3>
-                            <p className="statistic__desc">successful projects</p>
-                        </li>
-
-                    </ul>
-                </div>
-     
-        
-    )
-}
+        <li className="statistic__item">
+          <h3 className="statistic__title">70+</h3>
+          <p className="statistic__desc">successful projects</p>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default AboutUs;
